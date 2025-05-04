226c7a54263bab9fa99993d1d5f333730c741b4d
YTMini <minitech@YTMinis-Mac-mini.local>
2025-05-04 16:50:09 -0700

    first part of midterm, integer transformation

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 
 app = Flask(__name__)
 
 
-@app.route("/some_route_here", methods=["GET"])
-def method_name():
-    return {}
+@app.route("/api/calcs/<int:id>", methods=["GET"])
+def method_name(id):
+    number = int(id)
+    num_To_hex = hex(number)
+    decrement_num = number - 1
+
+    num_transformations = [{
+        "hex": num_To_hex,
+        "minus_one": decrement_num
+    }]
+    return jsonify(num_transformations),200
