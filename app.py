2dc27050dcc3ead3792dc202ee05c96e63d3922e
YTMini <minitech@YTMinis-Mac-mini.local>
2025-05-04 17:46:41 -0700

    first part of midterm complete

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 
 
 app = Flask(__name__)
 
 
-@app.route("/api/calcs/<int:id>", methods=["GET"])
-def method_name(id):
-    number = int(id)
-    num_To_hex = hex(number)
-    decrement_num = number - 1
-
-    num_transformations = [{
-        "hex": num_To_hex,
-        "minus_one": decrement_num
-    }]
+@app.route("/api/calcs/<id>", methods=["GET"])
+def integer_transformation(id):
+    try:
+        number = int(id)
+    except ValueError:
+        abort(400)
+    else:
+        number = int(id)
+        num_To_hex = hex(number)
+        decrement_num = number - 1
+        num_transformations = {
+            "hex": num_To_hex,
+            "dec": decrement_num
+        }
     return jsonify(num_transformations),200
